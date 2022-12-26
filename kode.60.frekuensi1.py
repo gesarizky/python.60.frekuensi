f09ac5b593eb64e094ea989e44991329faaff977
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-12-26 10:17:33 +0700

    Add files via upload

---
+++ kode.60.frekuensi1.py
@@ -0,0 +1,15 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+n = float(input("Masukan Nilai Jumlah Getaran : "))
+t = float(input("Masukan Nilai Waktu Getaran :  "))
+
+# Mengkonversi
+f = n / t
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Nilai Frekuensi = ',round(f,3))
+print('==========================================================')
