4b5bcd01553a9d2c7f022792cedae62a082bcc58
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2023-01-04 15:26:59 +0700

    Add files via upload

---
+++ kode.60.frekuensi2.py
@@ -0,0 +1,15 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+v = float(input("Masukan Nilai Kecepatan Gelombang (m/s) : "))
+l = float(input("Masukan Nilai Panjang Gelombang :  "))
+
+# Mengkonversi
+f = v / l
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Nilai Frekuensi = ',round(f,3))
+print('==========================================================')
