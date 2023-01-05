8286939d00590bd5600226bea7cda2cc45e119a6
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2023-01-05 13:02:21 +0700

    Add files via upload

---
+++ kode.60.frekuensi3.py
@@ -0,0 +1,14 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+p = float(input("Masukan Nilai Periode (s) : "))
+
+# Mengkonversi
+f = 1 / p
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Nilai Frekuensi (Hz) = ',round(f,3))
+print('==========================================================')
